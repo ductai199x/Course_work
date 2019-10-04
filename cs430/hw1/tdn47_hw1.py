d0f8df63c75ee7f56652fff8c5beff11a73e233a
Tai Nguyen <taiducnguyen.drexel@gmail.com>
2019-10-03 23:54:08 -0400

    finish cs430 hw1

--- cs430/hw1/tdn47_hw1.py
+++ cs430/hw1/tdn47_hw1.py
@@ -18,7 +18,6 @@ parser.add_argument("--stdout", help="print output or not", type=int, default=1)
 args = parser.parse_args()
 
 import numpy as np
-import math
 
 ## DEFINE GLOBAL VARIABLES
 window_size = (0,0)
@@ -32,16 +31,7 @@ class Point():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
-    def less_than(self, P):
-        if self.x > P.x and self.y > P.y:
-            return False
-        return True
-    
-    def greater_than(self, P):
-        if self.x < P.x and self.y < P.y:
-            return False
-        return True
+
 
 def write_xpm_header():
     global xpm
@@ -56,14 +46,13 @@ static char *sco100[] = {{
 "1 c #000000",
 /* pixels */
 '''.format(
-        # width=args.c-args.a+1, 
-        # height=args.d-args.b+1,
         width=picture.shape[1], 
         height=picture.shape[0],
         num_colors=2,
         chars_per_pixel=1,
         )
     xpm += xpm_header
+
 
 def write_xpm_pixels():
     global picture, xpm
@@ -77,6 +66,7 @@ def write_xpm_pixels():
 '''
 };
 '''
+
 
 # Using bresenham
 def create_line(lines):
@@ -96,10 +86,8 @@ def create_line(lines):
         D = 2*dy - dx
         while True:
             if dy >= 0:
-                # if x > args.c or y > args.d or x > r.x or y > r.y:
                 if x > r.x or y > r.y:
                     break
-                # if x >= args.a and y >= args.b:
                 picture[picture.shape[0]-1-y][x-1] = 1
                 if D <= 0:
                     D += 2*dy
@@ -114,10 +102,8 @@ def create_line(lines):
                     if dy < 0:
                         y -= 1
             else:
-                # if x > args.c or y < args.b or x > r.x or y < r.y:
                 if x > r.x or y < r.y:
                     break
-                # if x >= args.a and y <= args.d:
                 picture[picture.shape[0]-1-y][x-1] = 1
                 if D <= 0:
                     D -= 2*dy
@@ -162,15 +148,13 @@ def get_max_dim(lines):
                 lowest_y = l[3]
             if l[1] > highest_y:
                 highest_y = l[1]
-    print (lowest_x, lowest_y, highest_x, highest_y)
     return (highest_x-lowest_x, highest_y-lowest_y)
+
 
 def parse_ps():
     global picture
-    # /home/sweet/2-coursework/cs430/hw1.ps
-    # with open(args.f) as fp:
     lines = []
-    with open("/home/sweet/2-coursework/cs430/hw1.ps") as fp:
+    with open(args.f) as fp:
         is_started = 0
         for cnt, line in enumerate(fp):
             line = line.strip()
@@ -190,11 +174,22 @@ def parse_ps():
                 
     picture = np.zeros((max_y, max_x), dtype=int)
     create_line(lines)
-    print(picture.shape)
+
 
 def parse_window():
     global window_size
     window_size = (args.c-args.a, args.d-args.b)
+
+
+# This is a suplemental solution to multiplying 2 matrices (type=float) due to 
+# numpy 1.15 bug(https://github.com/numpy/numpy/issues/13426) -- fixed in 1.16.
+# Solution is from https://stackoverflow.com/questions/10508021/matrix-multiplication-in-pure-python
+def matmul(a,b):
+    zip_b = zip(*b)
+    zip_b = list(zip_b)
+    return [[sum(ele_a*ele_b for ele_a, ele_b in zip(row_a, col_b)) 
+             for col_b in zip_b] for row_a in a]
+
 
 def parse_transf():
     global trans_mat, picture
@@ -204,20 +199,23 @@ def parse_transf():
     sin = np.sin(rad)
     rot_mat = [[cos,sin,0], [-sin,cos,0], [0,0,1]]
     transl_mat = [[1,0,args.m], [0,1,-args.n], [0,0,1]]
-    trans_mat = np.matmul(transl_mat, rot_mat)
-    trans_mat = np.matmul(trans_mat, scale_mat)
+    # trans_mat = np.matmul(transl_mat, rot_mat)
+    # trans_mat = np.matmul(trans_mat, scale_mat)
+    trans_mat = matmul(transl_mat, rot_mat)
+    trans_mat = matmul(trans_mat, scale_mat)
 
     result_pic = np.zeros(picture.shape, dtype=int)
 
-    # k = picture.shape[0]-window_size[1]-lowest_y
-    k = window_size[1]-lowest_y
+    k = picture.shape[0]+lowest_y
     l = 0-lowest_x
 
     for r in range(0, len(picture)):
         for c in range(0, len(picture[0])):
+            if picture[r][c] == 0: continue
             idx_mat = [[c-l],[r-k],[1]]
-            idx_mat = np.matmul(trans_mat, idx_mat)
-            idx_mat = idx_mat.astype(int)
+            # idx_mat = np.matmul(trans_mat, idx_mat)
+            idx_mat = matmul(trans_mat, idx_mat)
+            idx_mat = np.array(idx_mat, dtype=int)
             new_r = idx_mat[1][0] + k
             new_c = idx_mat[0][0] + l
             if (0 <= new_r < picture.shape[0]) and (0 <= new_c < picture.shape[1]):
@@ -228,8 +226,11 @@ def parse_transf():
 
 def crop_final_pic():
     global picture
-    # picture = picture[args.b-lowest_y:window_size[1]-lowest_y, args.a-lowest_x:args.c-lowest_x]
-    picture = picture[115:115+349, 0:700]
+    x = 0 - lowest_x + args.a
+    y = picture.shape[0] + lowest_y - args.b
+    picture = picture[y - window_size[1]:y, x:x + window_size[0]]
+    print(picture.shape)
+
 
 def main():
     global xpm
@@ -241,6 +242,7 @@ def main():
     write_xpm_pixels()
     if args.stdout:
         print(xpm)
+
 
 if __name__ == "__main__":
     main()
