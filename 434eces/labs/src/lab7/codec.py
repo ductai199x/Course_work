d79ad248230f24252deed93f60b0db70d53ac930
Tai Nguyen <taiducnguyen.drexel@gmail.com>
2019-11-22 00:33:45 -0500

    nghich ngu 434

--- 434eces/labs/src/lab7/codec.py
+++ 434eces/labs/src/lab7/codec.py
@@ -1,7 +1,11 @@
 import librosa
 
 def encode(signal):
-    sr = 44100
+    fs = 44100
+    
+    
+    
+    
     e_sig = librosa.resample(signal.astype('float_'), sr, sr//2)
     return e_sig.astype('int16')
 
@@ -9,3 +13,21 @@ def decode(signal):
     sr = 44100
     d_sig = librosa.resample(signal.astype('float_'), sr//2, sr)
     return d_sig.astype('int16')
+
+
+
+
+
+
+
+
+
+
+def powerlaw_quantizer(x):
+    return sgn(x)*np.log(1 + 255*np.abs(x))/np.log(1 + 255)
+
+def sgn(x):
+    a = np.array(x, copy=True)
+    a[a < 0] = -1.0
+    a[a >= 0] = 1.0
+    return np.asarray(a)
