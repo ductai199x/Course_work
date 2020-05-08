b801bda2c1c04534b1764b1cf5f453bd51e9fe9e
Tai Duc Nguyen <taiducnguyen.drexel@gmail.com>
2020-05-08 10:45:36 -0400

    finish up 622 hw2, starting 580tecec hw3

--- 622ecec/2_hw/gaussian_elimination/gauss_eliminate.c
+++ 622ecec/2_hw/gaussian_elimination/gauss_eliminate.c
@@ -1,45 +1,102 @@
 /* Gaussian elimination code.
  * 
  * Author: Naga Kandasamy
- * Date of last update: April 29, 2020
+ * Date of last update: April 22, 2020
  *
  * Student names(s): FIXME
  * Date: FIXME
  *
  * Compile as follows: 
- * gcc -o gauss_eliminate gauss_eliminate.c compute_gold.c -fopenmp -std=c99 -Wall -O3 -lm
+ * gcc -o gauss_eliminate gauss_eliminate.c compute_gold.c -O3 -Wall -lpthread -lm
  */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <time.h>
-#include <sys/time.h>
 #include <string.h>
 #include <math.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <getopt.h>
 #include "gauss_eliminate.h"
 
 #define MIN_NUMBER 2
 #define MAX_NUMBER 50
 
+
+typedef struct thread_data_s {
+    int tid;                        /* The thread ID */
+    int num_threads;                /* Number of threads in the worker pool */
+	Matrix *U;
+} thread_data_t;
+
+typedef struct barrier_s {
+    pthread_mutex_t mutex;          /* Protects access to the counter */
+    pthread_cond_t wait;          /* Signals that barrier is safe to cross */
+    int counter;                /* The value itself */
+} barrier_t;
+
+int num_threads = 1;
+barrier_t barrier;  
+
 /* Function prototypes */
 extern int compute_gold(float *, int);
 Matrix allocate_matrix(int, int, int);
-void gauss_eliminate_using_omp(Matrix);
+void gauss_eliminate_using_pthreads(Matrix *);
 int perform_simple_check(const Matrix);
 void print_matrix(const Matrix);
 float get_random_number(int, int);
 int check_results(float *, float *, int, float);
 
+void barrier_sync(barrier_t *, int, int);
+void *pthread_gauss(void *args);
+
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s matrix-size\n", argv[0]);
-        fprintf(stderr, "matrix-size: width and height of the square matrix\n");
-        exit(EXIT_FAILURE);
-    }
+    int s = 4;
+    int t = 1;
+    int rpt_mode = 0;
 
-    int matrix_size = atoi(argv[1]);
+    int opt; 
+      
+    // put ':' in the starting of the 
+    // string so that program can  
+    // distinguish between '?' and ':'  
+    while((opt = getopt(argc, argv, ":s:t:r")) != -1)  
+    {  
+        switch(opt)  
+        { 
+            case 's':  
+                s = atoi(optarg);
+                break;  
+            case 't':  
+                t = atoi(optarg);
+                break;
+            case 'r':
+                rpt_mode = 1;
+                break;
+            case ':':  
+                printf("option needs a value\n");  
+                break;  
+            case '?':  
+                printf("unknown option: %c\n", optopt); 
+                break;  
+            default:
+				fprintf(stderr, "Usage: %s -s matrix_size -t num_threads -r (enable reporting mode)\n", argv[0]);
+                abort();
+        }  
+    }  
+	
+    int matrix_size = s; 
+    num_threads = t;
+
+    if (rpt_mode)
+        printf("%u\t%d\t", matrix_size, num_threads);
+
+    float exec_time = 0;
 
     Matrix A;			                                            /* Input matrix */
     Matrix U_reference;		                                        /* Upper triangular matrix computed by reference code */
@@ -63,12 +120,12 @@ int main(int argc, char **argv)
     fprintf(stderr, "\nPerforming gaussian elimination using reference code\n");
     struct timeval start, stop;
     gettimeofday(&start, NULL);
-    
     int status = compute_gold(U_reference.elements, A.num_rows);
-  
-    gettimeofday(&stop, NULL);
-    fprintf(stderr, "CPU run time = %0.2f s\n", (float)(stop.tv_sec - start.tv_sec\
-                + (stop.tv_usec - start.tv_usec) / (float)1000000));
+	gettimeofday(&stop, NULL);
+	exec_time = (float)(stop.tv_sec - start.tv_sec\
+                + (stop.tv_usec - start.tv_usec)/(float)1000000);
+    fprintf(stderr, "Execution time = %fs\n", exec_time);
+    printf("%f\t", exec_time);
 
     if (status < 0) {
         fprintf(stderr, "Failed to convert given matrix to upper triangular. Try again.\n");
@@ -82,10 +139,18 @@ int main(int argc, char **argv)
     }
     fprintf(stderr, "Single-threaded Gaussian elimination was successful.\n");
   
-    /* FIXME: Perform Gaussian elimination using OpenMP. 
+    /* FIXME: Perform Gaussian elimination using pthreads. 
      * The resulting upper triangular matrix should be returned in U_mt */
-    fprintf(stderr, "\nPerforming gaussian elimination using omp\n");
-    gauss_eliminate_using_omp(U_mt);
+    fprintf(stderr, "\nPerforming gaussian elimination using pthreads\n");
+
+    gettimeofday(&start, NULL);
+    gauss_eliminate_using_pthreads(&U_mt);
+    gettimeofday(&stop, NULL);
+	exec_time = (float)(stop.tv_sec - start.tv_sec\
+                + (stop.tv_usec - start.tv_usec)/(float)1000000);
+
+    fprintf(stderr, "Execution time = %fs\n", exec_time);
+    printf("%f\n", exec_time);
 
     /* Check if pthread result matches reference solution within specified tolerance */
     fprintf(stderr, "\nChecking results\n");
@@ -102,9 +167,98 @@ int main(int argc, char **argv)
 }
 
 
-/* FIXME: Write code to perform gaussian elimination using omp */
-void gauss_eliminate_using_omp(Matrix U)
+/* FIXME: Write code to perform gaussian elimination using pthreads */
+void gauss_eliminate_using_pthreads(Matrix *U)
 {
+    // initialize barrier sync
+	barrier.counter = 0;
+	pthread_mutex_init(&(barrier.mutex), NULL);
+	pthread_cond_init(&(barrier.wait), NULL);
+
+
+	// create threads
+	pthread_t *thread_id = (pthread_t *)malloc (num_threads * sizeof(pthread_t));
+
+    pthread_attr_t attributes;      /* Thread attributes */
+    pthread_attr_init(&attributes); /* Initialize thread attributes to default values */
+
+    /* Fork point: allocate memory on heap for required data structures and create worker threads */
+    thread_data_t *thread_data = (thread_data_t *) malloc(sizeof(thread_data_t) * num_threads);	  
+
+	int i;
+    for (i = 0; i < num_threads; i++) {
+        thread_data[i].tid = i; 
+        thread_data[i].num_threads = num_threads;
+        thread_data[i].U = U; 
+    }
+
+	for (i = 0; i < num_threads; i++)
+        pthread_create(&thread_id[i], &attributes, pthread_gauss, (void *)&thread_data[i]);
+
+	for (i = 0; i < num_threads; i++)
+        pthread_join(thread_id[i], NULL);
+
+	free(thread_id);
+    free(thread_data);
+
+}
+
+void *pthread_gauss(void *args)
+{
+    thread_data_t *thread_data = (thread_data_t *)args;
+
+    int i, j, k;
+    unsigned int n_rows = thread_data->U->num_rows;
+    unsigned int n_cols = thread_data->U->num_columns;
+    float* mat = thread_data->U->elements;
+
+    int tid = thread_data->tid;
+    int stride = thread_data->num_threads;
+    
+    
+    for (k = 0; k < n_rows; k++) {
+        int chunk_size = (int)floor(((float)n_cols-k-1)/(float)thread_data->num_threads); 
+        int offset = tid*chunk_size + k + 1;
+        int start = offset;
+        int finish = (tid < (thread_data->num_threads - 1)) ? offset+chunk_size : n_cols;
+        for (j = start; j < finish; j++) {
+            mat[n_cols * k + j] = (float)(mat[n_cols * k + j] / mat[n_cols * k + k]);
+        }
+
+        barrier_sync(&barrier, thread_data->tid, thread_data->num_threads);
+        
+        for (i = (k + tid + 1); i < n_rows; i+=stride) {
+            for (j = (k + 1); j < n_cols; j++) {
+                mat[n_cols * i + j] -= (mat[n_cols * i + k] * mat[n_cols * k + j]);	
+            }
+            mat[n_cols * i + k] = 0;
+        }
+
+        mat[n_cols * k + k] = 1;
+
+        barrier_sync(&barrier, thread_data->tid, thread_data->num_threads);
+    }
+
+    pthread_exit(NULL);
+
+    
+}
+
+/* Barrier synchronization implementation */
+void barrier_sync(barrier_t *barrier, int tid, int num_threads)
+{
+	pthread_mutex_lock(&(barrier->mutex));
+	if (barrier->counter == num_threads - 1) {		
+
+		barrier->counter = 0;
+		pthread_cond_broadcast(&(barrier->wait));
+	} 
+	else {
+		barrier->counter++;
+		pthread_cond_wait(&(barrier->wait), &(barrier->mutex));
+	}
+
+	pthread_mutex_unlock(&(barrier->mutex));
 }
 
 
@@ -157,4 +311,19 @@ int perform_simple_check(const Matrix M)
             return -1;
   
     return 0;
+}
+
+void print_matrix(const Matrix M)
+{
+    int i, j;
+	for (i = 0; i < M.num_rows; i++) {
+        for (j = 0; j < M.num_columns; j++) {
+			fprintf(stderr, "%f ", M.elements[i * M.num_rows + j]);
+        }
+		
+        fprintf(stderr, "\n");
+	} 
+	
+    fprintf(stderr, "\n");
+    return;
 }
--- 622ecec/2_hw/jacobi_solver/jacobi_solver.c
+++ 622ecec/2_hw/jacobi_solver/jacobi_solver.c
@@ -2,31 +2,97 @@
  * by iteration.
 
  * Author: Naga Kandasamy
- * Date modified: April 29, 2020
+ * Date modified: April 22, 2020
  *
  * Compile as follows:
- * gcc -o jacobi_solver jacobi_solver.c compute_gold.c -fopenmp -std=c99 -Wall -O3 -lm 
+ * gcc -o jacobi_solver jacobi_solver.c compute_gold.c -Wall -O3 -lpthread -lm 
+ * 
+ * Student names: Tai D. Nguyen
+ * Date: May 4, 2020
 */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
 #include <math.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <getopt.h>
 #include "jacobi_solver.h"
 
 /* Uncomment the line below to spit out debug information */ 
-/* #define DEBUG */
+// #define DEBUG
+
+#define TOLERANCE 0.00001
+
+typedef struct thread_data_s {
+    int tid;                        /* The thread ID */
+    int num_threads;                /* Number of threads in the worker pool */
+	matrix_t *A;
+	matrix_t *B;
+	matrix_t *X;
+	matrix_t *new_X;
+	double *partial_ssd;
+} thread_data_t;
+
+typedef struct barrier_s {
+    pthread_mutex_t mutex;          /* Protects access to the counter */
+    pthread_cond_t wait;          /* Signals that barrier is safe to cross */
+    int counter;                /* The value itself */
+} barrier_t;
+
+int num_threads = 1;
+barrier_t barrier;  
+
+void barrier_sync(barrier_t *, int, int);
+void *pthread_jacobi_solver(void *args);
 
 int main(int argc, char **argv) 
 {
-	if (argc < 2) {
-		fprintf(stderr, "Usage: %s matrix-size\n", argv[0]);
-        fprintf(stderr, "matrix-size: width of the square matrix\n");
-		exit(EXIT_FAILURE);
-	}
+	int s = 4;
+    int t = 1;
+    int rpt_mode = 0;
 
-    int matrix_size = atoi(argv[1]);
+    int opt; 
+      
+    // put ':' in the starting of the 
+    // string so that program can  
+    // distinguish between '?' and ':'  
+    while((opt = getopt(argc, argv, ":s:t:r")) != -1)  
+    {  
+        switch(opt)  
+        { 
+            case 's':  
+                s = atoi(optarg);
+                break;  
+            case 't':  
+                t = atoi(optarg);
+                break;
+            case 'r':
+                rpt_mode = 1;
+                break;
+            case ':':  
+                printf("option needs a value\n");  
+                break;  
+            case '?':  
+                printf("unknown option: %c\n", optopt); 
+                break;  
+            default:
+				fprintf(stderr, "Usage: %s -s matrix_size -t num_threads -r (enable reporting mode)\n", argv[0]);
+                abort();
+        }  
+    }  
+	
+    int matrix_size = s; 
+    num_threads = t;
+
+    if (rpt_mode)
+        printf("%u\t%d\t", matrix_size, num_threads);
+
+    float exec_time = 0;
 
     matrix_t  A;                    /* N x N constant matrix */
 	matrix_t  B;                    /* N x 1 b matrix */
@@ -34,7 +100,9 @@ int main(int argc, char **argv)
     matrix_t mt_solution_x;         /* Solution computed by pthread code */
 
 	/* Generate diagonally dominant matrix */
-    fprintf(stderr, "\nCreating input matrices\n");
+	if (!rpt_mode) {
+    	fprintf(stderr, "\nCreating input matrices\n");
+	}
 	srand(time(NULL));
 	A = create_diagonally_dominant_matrix(matrix_size, matrix_size);
 	if (A.elements == NULL) {
@@ -54,17 +122,41 @@ int main(int argc, char **argv)
 #endif
 
     /* Compute Jacobi solution using reference code */
-	fprintf(stderr, "Generating solution using reference code\n");
+	if (!rpt_mode) {
+		fprintf(stderr, "Generating solution using reference code\n");
+	}
     int max_iter = 100000; /* Maximum number of iterations to run */
+	struct timeval start, stop;	
+	gettimeofday(&start, NULL);
     compute_gold(A, reference_x, B, max_iter);
-    display_jacobi_solution(A, reference_x, B); /* Display statistics */
+	gettimeofday(&stop, NULL);
+	exec_time = (float)(stop.tv_sec - start.tv_sec\
+                + (stop.tv_usec - start.tv_usec)/(float)1000000);
+    if (!rpt_mode) {
+	    fprintf(stderr, "Execution time = %fs\n", exec_time);
+		display_jacobi_solution(A, reference_x, B); /* Display statistics */
+	} else {
+        printf("%f\t", exec_time);
+	}
+    
 	
-	/* Compute the Jacobi solution using openMP. 
-     * Solution is returned in mt_solution_x.
+	/* Compute the Jacobi solution using pthreads. 
+     * Solutions are returned in mt_solution_x.
      * */
-    fprintf(stderr, "\nPerforming Jacobi iteration using omp\n");
-	compute_using_omp(A, mt_solution_x, B);
-    display_jacobi_solution(A, mt_solution_x, B); /* Display statistics */
+	if (!rpt_mode) {
+    	fprintf(stderr, "\nPerforming Jacobi iteration using pthreads\n");
+	}
+	gettimeofday(&start, NULL);
+	compute_using_pthreads(&A, &mt_solution_x, &B);
+	gettimeofday(&stop, NULL);
+	exec_time = (float)(stop.tv_sec - start.tv_sec\
+                + (stop.tv_usec - start.tv_usec)/(float)1000000);
+    if (!rpt_mode) {
+	    fprintf(stderr, "Execution time = %fs\n", exec_time);
+		display_jacobi_solution(A, reference_x, B); /* Display statistics */
+	} else {
+        printf("%f\n", exec_time);
+	}
     
     free(A.elements); 
 	free(B.elements); 
@@ -74,10 +166,137 @@ int main(int argc, char **argv)
     exit(EXIT_SUCCESS);
 }
 
-/* FIXME: Complete this function to perform the Jacobi calculation using openMP. 
+/* FIXME: Complete this function to perform the Jacobi calculation using pthreads. 
  * Result must be placed in mt_sol_x. */
-void compute_using_omp(const matrix_t A, matrix_t mt_sol_x, const matrix_t B)
+
+void compute_using_pthreads (matrix_t *A, matrix_t *X, matrix_t *B)
 {
+	// create new_mt_sol_x
+	matrix_t *new_X = (matrix_t*)malloc(sizeof(matrix_t));
+	new_X->num_columns = X->num_columns;
+	new_X->num_rows = X->num_rows;
+
+	int size_X = X->num_columns*X->num_rows;
+	new_X->elements = (float*)malloc(sizeof(float)*size_X);
+
+	memcpy(new_X->elements, X->elements, size_X*sizeof(float));
+
+
+	// initialize barrier sync
+	barrier.counter = 0;
+	pthread_mutex_init(&(barrier.mutex), NULL);
+	pthread_cond_init(&(barrier.wait), NULL);
+
+
+	// create threads
+	pthread_t *thread_id = (pthread_t *)malloc (num_threads * sizeof(pthread_t));
+
+    pthread_attr_t attributes;      /* Thread attributes */
+    pthread_attr_init(&attributes); /* Initialize thread attributes to default values */
+	
+
+	// malloc for partial ssds
+	double *partial_ssd = (double*)malloc(sizeof(double)*num_threads);
+
+    /* Fork point: allocate memory on heap for required data structures and create worker threads */
+    thread_data_t *thread_data = (thread_data_t *) malloc(sizeof(thread_data_t) * num_threads);	  
+
+	int i;
+    for (i = 0; i < num_threads; i++) {
+        thread_data[i].tid = i; 
+        thread_data[i].num_threads = num_threads;
+        thread_data[i].A = A; 
+		thread_data[i].B = B; 
+        thread_data[i].X = X; 
+		thread_data[i].new_X = new_X;
+		thread_data[i].partial_ssd = partial_ssd;
+    }
+
+	for (i = 0; i < num_threads; i++)
+        pthread_create(&thread_id[i], &attributes, pthread_jacobi_solver, (void *)&thread_data[i]);
+
+	for (i = 0; i < num_threads; i++)
+        pthread_join(thread_id[i], NULL);
+
+	free(thread_id);
+    free(thread_data);
+
+}
+
+int done = 0;
+
+void *pthread_jacobi_solver(void *args)
+{
+	thread_data_t *thread_data = (thread_data_t *)args;
+
+	float sum = 0;
+	int sum_chunk_size = thread_data->A->num_columns;
+	
+	int stride_X = thread_data->num_threads;
+
+	while(!done) {
+
+		float tmp_ssd = 0;
+		double partial_ssd = 0;
+		for (int i = thread_data->tid; i < thread_data->A->num_rows; i+=stride_X) {
+			sum = 0;
+			for (int j = 0; j < sum_chunk_size; j++) {
+				if (i != j) {
+					sum += thread_data->A->elements[i*sum_chunk_size + j] * thread_data->X->elements[j];
+				}
+			}
+			thread_data->new_X->elements[i] = (thread_data->B->elements[i] - sum)/thread_data->A->elements[i*sum_chunk_size + i];
+			
+			tmp_ssd = thread_data->X->elements[i] - thread_data->new_X->elements[i];
+
+			partial_ssd += tmp_ssd*tmp_ssd;
+		}
+
+		thread_data->partial_ssd[thread_data->tid] = partial_ssd;
+
+		// barrier sync
+		barrier_sync(&barrier, thread_data->tid, thread_data->num_threads);
+
+		// do the sum ssds
+		if (thread_data->tid == 0) {
+			double sum_ssds = 0;
+			for (int i = 0; i < thread_data->num_threads; i++) {
+				sum_ssds += thread_data->partial_ssd[i];
+			}
+			if (sqrt(sum_ssds) < 0.00001) {
+				done = 1;
+			}
+		}
+
+		barrier_sync(&barrier, thread_data->tid, thread_data->num_threads);
+
+		if (!done) {
+			matrix_t *tmp_ptr;
+			tmp_ptr = thread_data->X;
+			thread_data->X = thread_data->new_X;
+			thread_data->new_X = tmp_ptr;
+		}
+	}
+
+	pthread_exit(NULL);
+
+}
+
+/* Barrier synchronization implementation */
+void barrier_sync(barrier_t *barrier, int tid, int num_threads)
+{
+	pthread_mutex_lock(&(barrier->mutex));
+	if (barrier->counter == num_threads - 1) {		
+
+		barrier->counter = 0;
+		pthread_cond_broadcast(&(barrier->wait));
+	} 
+	else {
+		barrier->counter++;
+		pthread_cond_wait(&(barrier->wait), &(barrier->mutex));
+	}
+
+	pthread_mutex_unlock(&(barrier->mutex));
 }
 
 /* Allocate a matrix of dimensions height * width.
@@ -109,7 +328,7 @@ void print_matrix(const matrix_t M)
     int i, j;
 	for (i = 0; i < M.num_rows; i++) {
         for (j = 0; j < M.num_columns; j++) {
-			fprintf(stderr, "%f ", M.elements[i * M.num_columns + j]);
+			fprintf(stderr, "%f ", M.elements[i * M.num_rows + j]);
         }
 		
         fprintf(stderr, "\n");
