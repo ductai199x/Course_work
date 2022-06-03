00fa6a48d06377d090952a37dbfae78b9d625ce7
Tai Nguyen <taiducnguyen.drexel@gmail.com>
2022-06-03 02:18:26 -0400

    add finished assignment 5

--- 691dsci/utilities.py
+++ 691dsci/utilities.py
@@ -241,6 +241,46 @@ def adagrad(f, x0, eta, m, handle, useSaved=False, save_every=10, print_every=10
 # Utilities 05
 def to_gpu(x):
     if torch.cuda.is_available():
-        return x.to("cuda:1")
+        return x.to("cuda")
 
     return x.to("cpu")
+
+
+class Vocab:
+
+    """
+    Class that handles mapping to and from
+    words to vocab indices
+    """
+
+    def __init__(self, stream="tokens", target=False):
+        self._target = target
+        if self._target:
+            self._word2idx = {}
+        else:
+            self._word2idx = {"__UNK__": 0, "": 1}
+        self._idx2word = {}
+        self._stream = stream
+
+    def train(self, raw_ds):
+        # generate vocab list
+        for sample in raw_ds:
+            for token in sample[self._stream]:
+                t = token.lower()
+                if t not in self._word2idx:
+                    self._word2idx[t] = len(self._word2idx)
+
+        # rebuild reverse lookup
+        self._idx2word = {v: k for k, v in self._word2idx.items()}
+
+    def encode(self, word):
+        if self._target:
+            return self._word2idx[word]
+        else:
+            return self._word2idx.get(word, self._word2idx["__UNK__"])
+
+    def decode(self, idx):
+        if self._target:
+            return self._idx2word[idx]
+        else:
+            return self._idx2word.get(idx, "__UNK__")
